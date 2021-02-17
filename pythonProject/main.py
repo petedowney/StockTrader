fb97cba26f7f621431fcd133bc1fb184dc5bc928
Peter Downey <peter.d.downey@gmail.com>
2021-02-17 12:26:02 -0500

    kinda just put the stuff in a method

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -11,27 +11,38 @@ import h5py
 import alpaca_trade_api as tradeapi
 import json
 
+def connectToAccount():
+    try:
+        print("Accessing Keys")
 
-try:
-    print("Accessing Keys")
+        with open("keys.json", "r") as read_file:
+            data = json.load(read_file)
 
-    with open("keys.json", "r") as read_file:
-        data = json.load(read_file)
+        print("Keys Found")
+        print("Accessing Account")
 
-    print("Keys Found")
-    print("Accessing Account")
+        api = tradeapi.REST(data["Public Key"], data["Private Key"], base_url='https://paper-api.alpaca.markets')
+        api.list_positions()
 
-    api = tradeapi.REST(data["Public Key"], data["Private Key"], base_url='https://paper-api.alpaca.markets')
-    account = api.get_account()
-    api.list_positions()
+        print("Account Accessed")
 
-    print("Account Accessed")
+        return api, api.get_account()
 
-except FileNotFoundError:
-    print("Keys not found")
-    exit(1)
+    except FileNotFoundError:
+        print("Keys not found")
+        exit(1)
 
-except tradeapi.rest.APIError:
-    print("Invalid Keys")
-    exit(2)
+    except tradeapi.rest.APIError:
+        print("Invalid Keys")
+        exit(2)
+
+
+api, account = connectToAccount()
+
+
+
+
+
+
+
 

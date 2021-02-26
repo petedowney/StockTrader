38d6569b136f35c107aa0b068cf42fe98f32ecdd
Longineese <50775507+Longineese@users.noreply.github.com>
2021-02-25 23:33:28 -0500

    stuff idk

--- pythonProject/neural_net.py
+++ pythonProject/neural_net.py
@@ -6,15 +6,18 @@ Created on Mon Feb 22 15:04:12 2021
 """
 import math
 import numpy as np
+from keras import backend as back
 from keras import models
 from keras import layers
 from keras import utils
-import tensorflow
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+
+
+import matplotlib.pyplot as plt
 
 
 def standerdize(data):
-
-    print(len(data))
 
     f = 0
     for n in data:
@@ -45,25 +48,34 @@ def splitData(data, yCount = 1, trainPercent = 0.9): # TODO: shuffle randomly US
     
     trainshape = train_X.shape
     train_X = train_X.reshape(trainshape[0], trainshape[1], 1)
-    train_Y = train_Y.reshape(trainshape[0])
+    train_Y = train_Y.reshape(trainshape[0], yCount)
 
     testshape = test_X.shape
     test_X = test_X.reshape(testshape[0], testshape[1], 1)
-    test_Y = test_Y.reshape(testshape[0])
+    test_Y = test_Y.reshape(testshape[0], yCount)
     
     return ((train_X, train_Y), (test_X, test_Y))
+
+'''
+print('GPU COUNT:', str(len(tf.config.experimental.list_physical_devices('GPU'))))
+print(device_lib.list_local_devices())
+
+tf.test.is_built_with_cuda()
+print('GPU COUNT:', str(len(tf.config.experimental.list_physical_devices('GPU'))))
+'''
+
+#print(back.tensorflow_backend._get_available_gpus())
 
 fileName = 'data.csv'
 raw_data = open(fileName, 'rt')
 data = np.loadtxt(raw_data, delimiter = ',', dtype = np.float)
 
-data = data[:500, :]
+data = data[:200, :]
 
+outputCount = 50
 
 data = standerdize(data)
-(train_X, train_Y), (test_X, test_Y) = splitData(data)
-
-print(data)
+(train_X, train_Y), (test_X, test_Y) = splitData(data, outputCount)
 
 model = models.Sequential()
 
@@ -71,37 +83,47 @@ model = models.Sequential()
 model.add(layers.LSTM(48, input_shape = (None, 1), activation = 'relu'))
 
 # hidden layers
-model.add(layers.Dense(138, activation='relu'))
+model.add(layers.Dense(128, activation='relu'))
 model.add(layers.Dense(64))
 
 # output layer
-model.add(layers.Dense(1, activation='linear')) # TODO: make output size 50
+model.add(layers.Dense(outputCount, activation='linear')) # TODO: make output size 50
 
 
 model.summary()
 
 
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
-model.fit(train_X, train_Y, epochs=20, batch_size=64)
+model.compile(loss='mean_squared_error', optimizer='adam'  , metrics=['accuracy'])
+model.fit(train_X, train_Y, epochs=50, batch_size=64)
 
 prediction = model.predict(test_X)
 
+def plot(preCount, outputCount, test_X, test_Y, prediction, index):
+    
+    test_X = test_X[index]
+    test_Y = test_Y[index]
+    prediction = prediction[index]
 
-#print("Correct".rjust(23), "Incorrect".rjust(10))
-print()
-t_Accuracy = 0
-for x in range(0, len(prediction[0])):
+    
+    fig = plt.figure()
+    ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    
+    ax.set(title = 'Predicted vs Actual', xlabel = 'sample', ylabel = 'value')
+        
+    
+    indices = range(len(test_X) - preCount, len(test_X) + outputCount)
+    postIndices = range(len(test_X), len(test_X) + outputCount)
+    
+    
+    ax.plot(indices, np.concatenate((test_X[-preCount:].flatten(), test_Y.flatten())), color = 'blue')
+    ax.plot(postIndices, prediction, color = 'orange')
+    
+    ax.axvline(x = len(test_X), color = 'green', linewidth = 2, linestyle = '--')
 
-    true = len(test_Y[x][abs(prediction[x] - test_Y[x]) <= 1]);
-    false = len(test_Y[x][abs(prediction[x] - test_Y[x]) > 1]);
+for i in range(len(test_X)):
+    plot(outputCount, outputCount, test_X, test_Y, prediction, i)
 
-    accuracy = true / len(test_Y[x])
-    t_Accuracy += accuracy;
 
-    print("Accuracy:".rjust(12), "{a:.3f}".format(a=accuracy).rjust(10), "row {a}".format(a=x + 1))
-
-t_Accuracy /= len(prediction[0])
-print("Average Accuracy:".rjust(12), "{a:.3f}".format(a=t_Accuracy).rjust(10))
 
 
 # use this:

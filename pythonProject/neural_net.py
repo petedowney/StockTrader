9a0f878768ddda57fc47da12236b5060a033c29d
Longineese <50775507+Longineese@users.noreply.github.com>
2021-03-04 15:05:42 -0500

    e

--- pythonProject/neural_net.py
+++ pythonProject/neural_net.py
@@ -11,7 +11,6 @@ from keras import layers
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 
 
 def smallestIndex(array):

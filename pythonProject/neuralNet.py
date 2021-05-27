8e8c8cefc1c2e2c3379280c90575d0cebe91d5fb
Peter Downey <peter.d.downey@gmail.com>
2021-05-27 12:38:36 -0400

    fixing stuff

--- pythonProject/neuralNet.py
+++ pythonProject/neuralNet.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 """
 Created on Mon Feb 22 15:04:12 2021
 
@@ -8,20 +8,25 @@ Created on Mon Feb 22 15:04:12 2021
 import numpy as np
 from keras import models
 from keras import layers
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 
-# DATA ===========
 from pythonProject import methods
 
+# DATA ===========
 file_name = 'data/techData.csv'
 raw_data = open(file_name, 'rt')
 data = np.loadtxt(raw_data, delimiter=',', dtype=np.float)
 
 output_count = 50
 
+methods.distributionPlotBefore(data)
+
 # standardization
 data, meanList, rangeList = methods.standerdize(data)
+
+methods.distributionPlotAfter(data)
 
 X, Y = methods.splitData(data, output_count + 1)
 
@@ -31,10 +36,11 @@ train_X, val_X, train_Y, val_Y = train_test_split(train_X, train_Y, test_size=0.
 
 reshaped = lambda x: x.reshape(x.shape[0], x.shape[1], 1)
 
-
+test_X = reshaped(test_X)
 train_X = reshaped(train_X)
 val_X = reshaped(val_X)
 
+test_Y, test_inverse = methods.snipY(test_Y)
 train_Y, train_inverse = methods.snipY(train_Y)
 val_Y, val_inverse = methods.snipY(val_Y)
 
@@ -59,6 +65,9 @@ model.summary()
 
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
+history = model.fit(train_X, train_Y, epochs=30, batch_size=64, validation_data=(val_X, val_Y))
+
+
 #code from:
 #https://machinelearningmastery.com/save-load-keras-deep-learning-models/
 model_json = model.to_json()
@@ -67,6 +76,4 @@ with open("data/model.json", "w") as json_file:
 # serialize weights to HDF5
 model.save_weights("data/model.h5")
 
-#plot history
-history = model.fit(train_X, train_Y, epochs=30, batch_size=64, validation_data=(val_X, val_Y))
-methods.plotLoss(history)
+print("Done")
